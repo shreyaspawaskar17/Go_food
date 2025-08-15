a409453816a8610aa371bcc4b626de675b3c61e8
Shreyas Praveen Pawaskar <115610072+shreyaspawaskar17@users.noreply.github.com>
2025-08-15 22:20:54 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT||5000;
 const mongoDB = require("./db");
 mongoDB();
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://go-food-1-z78l.onrender.com");
+    res.setHeader("Access-Control-Allow-Origin", "https://go-food-o8mg.onrender.com");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type,Accept"
