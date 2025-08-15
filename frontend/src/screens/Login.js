cb62ce9c79714e5b33499c1203f3bea6fc96ad70
Shreyas Praveen Pawaskar <115610072+shreyaspawaskar17@users.noreply.github.com>
2025-08-15 23:12:55 +0530

    Update Login.js

--- frontend/src/screens/Login.js
+++ frontend/src/screens/Login.js
@@ -7,7 +7,7 @@ export default function Login() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/loginuser", {
+    const response = await fetch("https://go-food-backend-k7r3.onrender.com/api/loginuser", {
       // credentials: 'include',
       // Origin:"http://localhost:3000/login",
       method: 'POST',
