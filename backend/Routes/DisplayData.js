5bd573d35cf4292d3e6061a2b84892befc6316fd
Shreyas Praveen Pawaskar <115610072+shreyaspawaskar17@users.noreply.github.com>
2025-08-19 22:05:03 +0530

    Update DisplayData.js

--- backend/Routes/DisplayData.js
+++ backend/Routes/DisplayData.js
@@ -4,12 +4,8 @@ const { FoodItem, FoodCategory } = require('../models/FoodItem');  // import bot
 
 router.get('/foodData', async (req, res) => {
   try {
-    const foodItems = await FoodItem.find({});
-    const foodCategories = await FoodCategory.find({});
+     res.send([global.food_items,global.food_category]);
 
-    
-
-    res.status(200).json([foodItems, foodCategories]);
   } catch (error) {
     console.error("Error fetching food data:", error);
     res.status(500).json({ message: "Server error", error: error.message });
